e4d77322049c88210a1bc501760c90d2675b8e04
Álex González <agonzalezro@gmail.com>
2016-06-28 00:39:54 +0200

    Tests for main module

--- test/properties_test.exs
+++ test/properties_test.exs
@@ -1,8 +1,32 @@
 defmodule PropertiesTest do
-  use ExUnit.Case
-  doctest Properties
+  use ExUnit.Case, async: true
+  import ExUnit.CaptureIO
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import Properties
+
+  test "add commute time to the property struct" do
+    assert add_commute_time(Property.new(%{})).commute_time == 3077
+  end
+
+  test "filter by parking" do
+    assert filter_by_parking(Property.new(%{})) == false
+  end
+
+  test "filter by commute time" do
+    p = %{Property.new(%{}) | commute_time: 1}
+    assert filter_by_commute(p) == true
+
+    p = %{p | commute_time: Application.get_env(:properties, :max_commute_time) + 1}
+    assert filter_by_commute(p) == false
+  end
+
+  test "value extractor" do
+    p = %{a: 1, b: 2}
+    assert clean_property(p, [:a, :b]) == [1, 2]
+  end
+
+  test "csv exporter" do
+    p = Property.new(address: "something")
+    assert capture_io(fn -> to_csv([p]) end) == "address,bathrooms,commute_time,floor,latitude,longitude,newDevelopment,price,priceByArea,rooms,size,thumbnail,url\nsomething,0,0,0,0.0,0.0,false,0,0,0,0,,\n"
   end
 end
